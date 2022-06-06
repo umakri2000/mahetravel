599e491000cdf1b2d4bdd4b4a1faf8b9224030a2
umakri2000 <krishnan89037@gmail.com>
2022-06-06 20:35:54 +0530

    code smell

--- travelsite/src/app/home/tourmunnar/tourmunnar.component.ts
+++ travelsite/src/app/home/tourmunnar/tourmunnar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,MinLengthValidator,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 import { ApiserviceService } from '../../apiservice.service';
 
 @Component({
